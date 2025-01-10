8c090bcb570defbc686a65161b6446c2a408c02b
Enveu A0552 <sonukr0157@gmail.com>
2025-01-10 16:45:55 +0530

    aad changes

--- src/main/java/com/Student/Relation/Student/Relationship/Controller/SubjectController.java
+++ src/main/java/com/Student/Relation/Student/Relationship/Controller/SubjectController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/subjects")
 public class SubjectController {
-
+dfghjkkl
     @Autowired
     private SubjectService subjectService;
 
